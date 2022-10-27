b43fabb062f4b5eed84a601308cf425354612c3f
Kalyan G <gkalyan0510@gmail.com>
2022-10-27 01:39:53 -0400

    remove unused variables

--- city-modeling/src/shader/normal.frag.glsl
+++ city-modeling/src/shader/normal.frag.glsl
@@ -22,17 +22,5 @@ void main()
 
     // diffuse
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-
-    // specular
-    float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
-
-    vec3 result = ( diffuse ) * objectColor;
     FragColor = vec4(abs(norm), 1.0);
 }
